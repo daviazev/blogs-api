80ae1590455e8115762f55ad244b636c754b6da6
daviazevdo <azvd.davi@gmail.com>
2022-11-30 14:45:27 -0300

    primeiro commit

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(express.json());
 
-// ...
+// ... vai brasiiiiiiiiil 🇧🇷🇧🇷🇧🇷🇧🇷🇧🇷🇧🇷🇧🇷🇧🇷
 
 // É importante exportar a constante `app`,
 // para que possa ser utilizada pelo arquivo `src/server.js`
